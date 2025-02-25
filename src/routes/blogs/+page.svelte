6a3544a45d173be46be7649fb76db5c4e16e5ce9
Param Pal Singh <singhparampal97@gmail.com>
2025-02-26 00:09:55 +0100

    add initial search form for blogs

--- src/routes/blogs/+page.svelte
+++ src/routes/blogs/+page.svelte
@@ -1,7 +1,8 @@
 <script>
   import BlogCard from "$lib/components/blog_card.svelte";
   import { Button } from "$lib/components/ui/button";
-  import Input from "$lib/components/ui/input/input.svelte";
+  import { Input } from "$lib/components/ui/input";
+  import { SearchIcon } from "lucide-svelte";
   import { fly } from "svelte/transition";
   let { data } = $props();
 </script>
@@ -11,15 +12,15 @@
   out:fly={{ x: 200, duration: 300 }}
   class="col-span-1 col-start-2 flex flex-col gap-8"
 >
-  <h1 class="text-5xl font-bold font-body">Blog</h1>
+  <h1 class="text-5xl font-bold font-body">Blogs</h1>
   <p class="font-body text-gray-500">
     There are {data.blog_infos.length} articles on this site. Use tags to get articles
     based on different tags. Use the search below to filter by title. >
   </p>
 
-  <form class="flex w-full max-w-sm items-center space-x-2">
-    <Input type="email" placeholder="email" />
-    <Button type="submit">Subscribe</Button>
+  <form class="flex w-full items-center space-x-2">
+    <Input type="email" placeholder="Search articles" />
+    <Button variant="outline" type="submit" size="icon"><SearchIcon /></Button>
   </form>
 
   <div>
